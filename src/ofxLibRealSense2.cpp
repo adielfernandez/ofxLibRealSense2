c77c934694934c44f18ddea366f1e12e668e9d7d
adielfernandez <adiel@bluecadet.com>
2019-11-15 15:46:38 -0500

    Revert "Dump of changes"
    This reverts commit 5ed17bd12c96070839b06ad971029b2c0f4d2840.

--- src/ofxLibRealSense2.cpp
+++ src/ofxLibRealSense2.cpp
@@ -10,17 +10,18 @@
 #include "ofLog.h"
 
 using namespace::std;
-using namespace::rs2;
 
-int ofxLibRealSense2::getDeviceCount() {
-	// query device
-	rs2::context ctx;
-	return ctx.query_devices().size();
+int ofxLibRealSense2::getDeviceCount()
+{
+    // query device
+    rs2::context ctx;
+    return ctx.query_devices().size();
 }
+
 
 void ofxLibRealSense2::setupDevice(int deviceID)
 {
-    // query device 
+    // query device
     rs2::context ctx;
     _deviceList = ctx.query_devices();
     cout << "RealSense device count: " << _deviceList.size() << endl;
@@ -33,25 +34,15 @@ void ofxLibRealSense2::setupDevice(int deviceID)
         ofSystemAlertDialog("Requested device id is invalid");
         std::exit(0);
     }
-
-	cout << "Devices List:\n-------------" << endl;
-	for (int i = 0; i < _deviceList.size(); i++) {
-		cout << i << ": " << _deviceList[i].get_info(RS2_CAMERA_INFO_NAME) << endl;
-	}
     
     string deviceSerial = _deviceList[deviceID].get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
-	string name = _deviceList[deviceID].get_info(RS2_CAMERA_INFO_NAME);
-
-    _curDeviceID = deviceID;
-	cout << "Attempting to connect to device: \n\t---ID: " << deviceID << "\n\t---Serial: " << deviceSerial << "\n\t---Name: " << name << endl;
+    _config.enable_device(deviceSerial);
+    cout << "Device name is: " << _deviceList[deviceID].get_info(RS2_CAMERA_INFO_NAME) << endl;
     
-	_config.enable_device(deviceSerial);
-
+    _curDeviceID = deviceID;
     _setupFinished = true;
     
     setupGUI();
-
-
 }
 
 
@@ -79,135 +70,24 @@ void ofxLibRealSense2::setupDepth(int width, int height, int fps)
 {
     _depthWidth = width;
     _depthHeight = height;
-    _depthTex.allocate(_depthWidth, _depthHeight, GL_LUMINANCE16);
+    _depthTex.allocate(_depthWidth, _depthHeight, GL_LUMINANCE);
     _config.enable_stream(RS2_STREAM_DEPTH, -1, _depthWidth, _depthHeight, RS2_FORMAT_Z16, fps);
     _depthEnabled = true;
-
-	mappedDepthPix.allocate(_depthWidth, _depthHeight, OF_IMAGE_GRAYSCALE);
-
-	//go 5 levels up from data folder, back down to addon folder
-	string addonPath = ofFilePath::getAbsolutePath("../../../../../addons/ofxLibRealSense2/shaders/");
-	string shaderName = "map";
-	mapShader.load( addonPath + shaderName );
-
-	mapFbo.allocate(_depthWidth, _depthHeight, GL_LUMINANCE);
-
-	mapFbo.begin();
-	ofClear(0);
-	mapFbo.end();
-
-}
-
-void ofxLibRealSense2::setupPointcloud() {
-
-	mesh.setMode(OF_PRIMITIVE_POINTS);
-	
-	_pointcloudEnabled = true;
-
 }
 
 
-void ofxLibRealSense2::startPipeline(bool useThread, Preset p )
+void ofxLibRealSense2::startPipeline(bool useThread)
 {
+//    _config.enable_device(_device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
     _pipeline.start(_config);
     _pipelineStarted=true;
-	
-	_preset = p;
-	setSensorPreset( _preset );
     
     _useThread = useThread;
     if(_useThread)
         startThread();
 }
 
-void ofxLibRealSense2::setSensorPreset(Preset p) {
 
-	rs2_rs400_visual_preset preset;
-
-	switch (p) {
-	case ofxLibRealSense2::Preset::DEFAULT:
-		preset = rs2_rs400_visual_preset::RS2_RS400_VISUAL_PRESET_DEFAULT;
-		break;
-	case ofxLibRealSense2::Preset::HIGH_ACCURACY:
-	preset = rs2_rs400_visual_preset::RS2_RS400_VISUAL_PRESET_HIGH_ACCURACY;
-		break;
-	case ofxLibRealSense2::Preset::HIGH_DENSITY:
-	preset = rs2_rs400_visual_preset::RS2_RS400_VISUAL_PRESET_HIGH_DENSITY;
-		break;
-	case ofxLibRealSense2::Preset::MEDIUM_DENSITY:
-	preset = rs2_rs400_visual_preset::RS2_RS400_VISUAL_PRESET_MEDIUM_DENSITY;
-		break;
-	case ofxLibRealSense2::Preset::HAND:
-	preset = rs2_rs400_visual_preset::RS2_RS400_VISUAL_PRESET_HAND;
-		break;
-	case ofxLibRealSense2::Preset::REMOVE_IR_PATTERN:
-	preset = rs2_rs400_visual_preset::RS2_RS400_VISUAL_PRESET_REMOVE_IR_PATTERN;
-		break;
-	default:
-		break;
-	}
-
-	auto sensor = _pipeline.get_active_profile().get_device().first<rs2::depth_sensor>();
-	sensor.set_option(rs2_option::RS2_OPTION_VISUAL_PRESET, preset);
-
-	_preset = p;
-
-}
-
-void ofxLibRealSense2::setSensorPreset( int i ) {
-	if( i >= 0 && i <= 5 ) {
-		setSensorPreset( (Preset)i );
-	} else {
-		return;
-		cout << "Not a valid preset" << endl;
-	}
-}
-
-void ofxLibRealSense2::setNextPreset() {
-	if ((int)_preset >= NUM_PRESETS - 1) {
-		setSensorPreset( 0 );
-	} else {
-		setSensorPreset(_preset + 1);
-	}
-}
-
-void ofxLibRealSense2::setPreviousPreset() {
-	if ((int)_preset <= 0) {
-		setSensorPreset( NUM_PRESETS - 1 );
-	} else {
-		setSensorPreset(_preset - 1);
-	}
-}
-
-string ofxLibRealSense2::getCurrentPresetName() {
-	string s;
-
-	switch (_preset) {
-	case ofxLibRealSense2::DEFAULT:
-		s = "DEFAULT";
-		break;
-	case ofxLibRealSense2::HIGH_ACCURACY:
-		s = "HIGH_ACCURACY"; 
-		break;
-	case ofxLibRealSense2::HIGH_DENSITY:
-		s = "HIGH_DENSITY"; 
-		break;
-	case ofxLibRealSense2::MEDIUM_DENSITY:
-		s = "MEDIUM_DENSITY"; 
-		break;
-	case ofxLibRealSense2::HAND:
-		s = "HAND"; 
-		break;
-	case ofxLibRealSense2::REMOVE_IR_PATTERN:
-		s = "REMOVE_IR_PATTERN"; 
-		break;
-	default:
-		break;
-	}
-
-	return s;
-
-}
 
 void ofxLibRealSense2::threadedFunction()
 {
@@ -225,62 +105,27 @@ void ofxLibRealSense2::updateFrameData()
 {
     rs2::frameset frameset;
     if(_pipeline.poll_for_frames(&frameset)) {
-        
-		rs2::video_frame colFrame = frameset.get_color_frame();
-        rs2::video_frame irFrame = frameset.get_infrared_frame();
-        rs2::depth_frame depthFrame = frameset.get_depth_frame();
-        
-		if(_colorEnabled) {
+        if(_colorEnabled) {
+            rs2::video_frame colFrame = frameset.get_color_frame();
             _colBuff = (uint8_t*)colFrame.get_data();
             _colorWidth = colFrame.get_width();
             _colorHeight = colFrame.get_height();
             _hasNewColor = true;
         }
         if(_irEnabled) {
+            rs2::video_frame irFrame = frameset.get_infrared_frame();
             _irBuff = (uint8_t*)irFrame.get_data();
             _irWidth = irFrame.get_width();
             _irHeight = irFrame.get_height();
             _hasNewIr = true;
         }
         if(_depthEnabled) {
+            rs2::depth_frame depthFrame = frameset.get_depth_frame();
             _depthBuff = (uint16_t*)depthFrame.get_data();
             _depthWidth = depthFrame.get_width();
             _depthHeight = depthFrame.get_height();
             _hasNewDepth = true;
         }
-		
-		if (_pointcloudEnabled && _colorEnabled && _depthEnabled) {
-
-			auto points = pointcloud.calculate(depthFrame);
-
-			// Tell pointcloud object to map to this color frame
-			pointcloud.map_to(colFrame);
-
-			auto vertices = points.get_vertices();
-			auto tex_coords = points.get_texture_coordinates();
-
-			mesh.clear();
-
-			for (int i = 0; i < points.size(); i++) {
-				if (vertices[i].z) {
-
-					ofVec3f p(vertices[i].x, vertices[i].y, vertices[i].z);
-					ofVec2f t(tex_coords[i].u, tex_coords[i].v);
-
-					mesh.addVertex( p );
-					mesh.addTexCoord( t );
-
-				}
-			}
-
-			ofNotifyEvent(newDepthEvent, this);
-		}
-
-		//do a little averaging for a more readable framerate
-		double frameTime = ofGetElapsedTimef();
-		double dt = frameTime - lastFrameTime;
-		framerate = 1.0f/dt;
-		lastFrameTime = frameTime;
     }
 }
 
@@ -294,75 +139,10 @@ void ofxLibRealSense2::update()
         updateFrameData();
     }
     
-    _hasNewFrame = _hasNewColor || _hasNewIr || _hasNewDepth;
+    _hasNewFrame = _hasNewColor | _hasNewIr | _hasNewDepth;
 
     if(_depthBuff && _hasNewDepth) {
-
-		rawDepthPix.setFromPixels(_depthBuff, _depthWidth, _depthHeight, OF_IMAGE_GRAYSCALE);
-		_depthTex.loadData(rawDepthPix);
-
-
-		if (_mapDepth) {
-
-			if ( !_bUseDepthShader) {
-
-				//Map the short pixel data to an 8-bit gray scale.
-				//SLOW!!! Move this to a shader
-				for (int i = 0; i < _depthWidth * _depthHeight; i++) {
-
-					if (rawDepthPix[i] == 0) {
-						mappedDepthPix[i] = 0;
-					} else {
-						mappedDepthPix[i] = ofMap(rawDepthPix[i], _nearLimit, _farLimit, 255.0f, 0.0f, true);
-					}
-
-				}
-				
-				mappedDepthTex.loadData(mappedDepthPix);
-
-			} else {
-
-				//use shader to get mapped depth
-				mapFbo.begin();
-
-				ofClear(255);
-
-				mapShader.begin();
-				mapShader.setUniformTexture("depthTex", _depthTex, 0);
-				mapShader.setUniform2f("resolution", mapFbo.getWidth(), mapFbo.getHeight());
-
-				//the values of the raw depth are uint16_t (unsigned short), but appear as normalized 0-1 values
-				//in the fragment shader, so convert the limits to normalized values too
-				float nearLim = ofMap(_nearLimit, 0, USHRT_MAX, 0.0f, 1.0f);
-				float farLim = ofMap(_farLimit, 0, USHRT_MAX, 0.0f, 1.0f);
-
-				mapShader.setUniform1f("nearLimit", nearLim);
-				mapShader.setUniform1f("farLimit", farLim);
-
-				ofPushStyle();
-				ofSetColor(0);
-				ofFill();
-				ofDrawRectangle(0, 0, mapFbo.getWidth(), mapFbo.getHeight());
-				ofPopStyle();
-
-				//_depthTex.draw(0,0);
-
-				mapShader.end();
-
-				mapFbo.end();
-
-
-
-				mappedDepthTex = mapFbo.getTexture();
-				mapFbo.readToPixels(mappedDepthPix);
-
-			}
-
-
-			//notify listeners of new data
-			ofNotifyEvent(newMappedPixelsEvt, mappedDepthPix, this);
-
-		}
+        _depthTex.loadData(_depthBuff, _depthWidth, _depthHeight, GL_LUMINANCE);
         _hasNewDepth = false;
     }
 
@@ -388,10 +168,6 @@ void ofxLibRealSense2::setupGUI()
     _D400Params.add( _autoExposure.setup("Auto exposure", true) );
     _D400Params.add( _enableEmitter.setup("Emitter", true) );
     _D400Params.add( _irExposure.setup("IR Exposure", orExp.def, orExp.min, orExp.max ));
-	_D400Params.add( _mapDepth.setup("Map Depth Image", true)); 
-	_D400Params.add( _bUseDepthShader.setup("Use Depth Shader", true));
-	_D400Params.add( _nearLimit.setup("Near Limit", 350, 0, 8000));
-	_D400Params.add( _farLimit.setup("Far Limit", 1000, 0, 8000));
     
     _autoExposure.addListener(this, &ofxLibRealSense2::onD400BoolParamChanged);
     _enableEmitter.addListener(this, &ofxLibRealSense2::onD400BoolParamChanged);
--- src/ofxLibRealSense2.hpp
+++ src/ofxLibRealSense2.hpp
@@ -6,45 +6,29 @@
 //
 
 #pragma once
-#include "librealsense2/rs.hpp"
+#include <librealsense2/include/librealsense2/rs.hpp>
 #include "ofThread.h"
 #include "ofTexture.h"
 #include "ofxGui.h"
-
-#include "ofxAutoReloadedShader.h"
 
 class ofxLibRealSense2 : public ofThread
 {
     
 public:
-
-	enum Preset {
-		DEFAULT = 0,
-		HIGH_ACCURACY = 1,
-		HIGH_DENSITY = 2,
-		MEDIUM_DENSITY = 3,
-		HAND = 4,
-		REMOVE_IR_PATTERN = 5
-	};
-
-
     void setupDevice(int deviceID);
     void setupColor(int width, int height, int fps=60);
     void setupIR(int width, int height, int fps=60);
     void setupDepth(int width, int height, int fps=60);
-	void setupPointcloud();
-    void startPipeline(bool useThread, Preset p = Preset::DEFAULT );
+    void startPipeline(bool useThread);
     void update();
     void exit();
-
-	int getDeviceCount();
+    
+    int getDeviceCount();
     
     ofTexture*  getColorTex()   { return &_colTex; }
     ofTexture*  getIrTex()      { return &_irTex; }
     ofTexture*  getDepthTex()   { return &_depthTex; }
-	ofTexture*  getMappedDepthTex() { return &mappedDepthTex; }
-	ofVboMesh*	getPointCloudRef() { return &mesh; }
-
+    
     int getColorWidth() { return _colorWidth; }
     int getColorHeight(){ return _colorHeight; }
     int getIrWidth()    { return _irWidth; }
@@ -52,40 +36,12 @@ public:
     int getDepthWidth() { return _depthWidth; }
     int getDepthHeight(){ return _depthHeight; }
     bool isFrameNew()   { return _hasNewFrame; }
-	float getFrameRate() { return framerate; };
-
+    
     ofxGuiGroup *getGui();
     
     ofxLibRealSense2() : _setupFinished(false), _colorEnabled(false), _irEnabled(false), _depthEnabled(false), _pipelineStarted(false), _useThread(false) {}
-
-
-	ofPixels mappedDepthPix;
-	ofShortPixels rawDepthPix;
-	ofVboMesh mesh;
-
-	ofEvent<ofPixels> newMappedPixelsEvt;
-	ofEvent<void> newDepthEvent;
-
-	void useDepthShader(bool use) { _bUseDepthShader = use; };
-	void setSensorPreset( Preset p );
-	void setSensorPreset( int i );
-	int getNumPresets() { return NUM_PRESETS; };
-	void setNextPreset();
-	void setPreviousPreset();
-	Preset getCurrentPreset() { return _preset; };
-	string getCurrentPresetName();
-
+    
 private:
-
-
-	ofxAutoReloadedShader mapShader;
-	ofFbo mapFbo;
-
-	Preset			_preset;
-	const int NUM_PRESETS = 6;
-
-
-	bool				bValidCameraFound;
     rs2::device_list    _deviceList;
     int             _curDeviceID;
     
@@ -94,7 +50,7 @@ private:
     bool            _useThread;
     bool            _setupFinished;
     bool            _pipelineStarted;
-    bool            _colorEnabled, _irEnabled, _depthEnabled, _pointcloudEnabled;
+    bool            _colorEnabled, _irEnabled, _depthEnabled;
     int             _colorWidth, _irWidth, _depthWidth;
     int             _colorHeight, _irHeight, _depthHeight;
     
@@ -107,27 +63,10 @@ private:
     ofxToggle       _autoExposure;
     ofxToggle       _enableEmitter;
     ofxIntSlider    _irExposure;
-	ofxToggle       _mapDepth;
-	ofxToggle		_bUseDepthShader;
-	ofxIntSlider    _nearLimit;
-	ofxIntSlider    _farLimit;
     
     void threadedFunction();
     void updateFrameData();
     void setupGUI();
     void onD400BoolParamChanged(bool &value);
     void onD400IntParamChanged(int &value);
-
-	//-----------------------
-	//----------NEW----------
-	//-----------------------
-
-	double lastFrameTime = 0;
-	
-	float framerate;
-
-	ofTexture mappedDepthTex;
-
-	rs2::pointcloud pointcloud;
-
 };
